5690283dc5e3fbf4fee8edce3d358482497a9a67
Jared Eklin <jaredeklin@gmail.com>
2018-04-07 12:14:07 -0600

    Refactor default image logic to ternary

--- src/TonightCard.js
+++ src/TonightCard.js
@@ -4,16 +4,12 @@ import defaultImage from './images/black-woven.jpg';
 
 export const TonightCard = (props) => {
   let { image, artist, venue, date } = props.show;
-  if (!image) {
-    image = defaultImage;
-  }
-  console.log(image)
 
   return (
     <article className="tonight-card">
-      <img src={image} className='artist-image' alt="artist image"/>
-      <h4 className="artist">{artist}</h4>
-      <h4 className="venue">{venue}</h4>
+      <img src={ image ? image : defaultImage } className='artist-image' alt="artist image"/>
+      <h4 className="artist">{ artist }</h4>
+      <h4 className="venue">{ venue }</h4>
     </article>
   )
 }
