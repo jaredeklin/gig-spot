299b2545aa122274d2686c7995cbe8fdb7047f7f
Jared Eklin <jaredeklin@gmail.com>
2018-04-07 15:11:54 -0600

    Refactor handleSubmit

--- src/containers/LocationForm/LocationForm.js
+++ src/containers/LocationForm/LocationForm.js
@@ -10,93 +10,12 @@ export class LocationForm extends Component {
     this.state = { 
       zipCode: '',
       radius: ''
-    }
+    }  
+  };
 
-    this.dirtyDate = "2018-04-20T21:00:00"
-  }
-
-  cleanDate = (dirtyDate) => {
-    const currentDateISO = new Date().toISOString()
-
-  }
-
-  // convertMonth = (month) => {
-  //   switch(month) {
-  //   case '1':
-  //     return 'Jan';
-
-  //   case 'Jan':
-  //     return '01';
-
-  //   case '2':
-  //     return 'Feb';
-
-  //   case 'Feb':
-  //     return '02';
-
-  //   case '3':
-  //     return 'Mar';
-
-  //   case 'Mar':
-  //     return '03';
-
-  //   case '4':
-  //     return 'Apr';
-
-  //   case 'Apr':
-  //     return '04';
-
-  //   case '5':
-  //     return 'May';
-
-  //   case 'May':
-  //     return '05';
-
-  //   case '6':
-  //     return 'Jun';
-
-  //   case 'Jun':
-  //     return '06';
-
-  //   case '7':
-  //     return 'Jul';
-
-  //   case 'Jul':
-  //     return '07';
-
-  //   case '8':
-  //     return 'Aug';
-
-  //   case 'Aug':
-  //     return '08';
-
-  //   case '9':
-  //     return 'Sep';
-
-  //   case 'Sep':
-  //     return '09';
-
-  //   case '10':
-  //     return 'Oct';
-
-  //   case 'Oct':
-  //     return '10';
-
-  //   case '11':
-  //     return 'Nov'; 
-
-  //   case 'Nov':
-  //     return '11';
-
-  //   case '12':
-  //     return 'Dec';
-
-  //   case 'Dec':
-  //     return '12';
-
-  //   default:
-  //     return
-  //   }
+  // cleanDate = () => {
+  //   const dirtyISODate = "2018-04-20T21:00:00"
+  //   const currentDateISO = new Date().toISOString()
   // }
 
   handleChange = (event) => {
@@ -109,15 +28,12 @@ export class LocationForm extends Component {
 
   handleSubmit = async (event) => {
     event.preventDefault();
-    const { loadShows, setLocation } = this.props;
+    const { loadShows, setLocation, history } = this.props;
     const shows = await fetchShows(this.state);
 
     loadShows(shows);
     setLocation(this.state);
-
-
-    const path = './main';
-    this.props.history.push(path);
+    history.push('./main');
   };
 
   render() {
