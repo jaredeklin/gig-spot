73d007aa5836d4dd3fb7a3ab2dff37a56ca8cae3
Jared Eklin <jaredeklin@gmail.com>
2018-04-07 15:14:34 -0600

    lint tonight card

--- src/components/TonightCard/TonightCard.js
+++ src/components/TonightCard/TonightCard.js
@@ -4,7 +4,8 @@ import defaultImage from '../../images/black-woven.jpg';
 
 export const TonightCard = (props) => {
   let { image, artist, venue, date } = props.show;
-  const formatedDate = `${date.month} ${date.day} ${date.startTime}`
+  const formatedDate = `${date.month} ${date.day} ${date.startTime}`;
+
   return (
     <article className="tonight-card">
       <img src={ image ? image : defaultImage } className='artist-image' alt="artist image"/>
@@ -12,5 +13,5 @@ export const TonightCard = (props) => {
       <h4 className="venue">{ venue }</h4>
       <h4 className="date">{ formatedDate }</h4>
     </article>
-  )
-}
+  );
+};
