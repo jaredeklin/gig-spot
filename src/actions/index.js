c3b492054e369b042182cc8f57c995a0eaa7e73f
Jared Eklin <jaredeklin@gmail.com>
2018-04-11 09:52:58 -0600

    Combine show reducers into one file, add opacity on hover to cards

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+
+
 export const loadTonightsShows = (shows) => ({
   type: "LOAD_TONIGHTS_SHOWS",
   shows
@@ -17,3 +19,27 @@ export const setLocation = (location) => ({
   type: "SET_LOCATION",
   location
 });
+
+// export const fetchShows = (location) => {
+//   const { zipCode, radius } = location;
+
+//   return (dispatch) => {
+//     fetch(`http://api.jambase.com/events?zipCode=${zipCode}&radius=${radius}&page=0&api_key=${jambaseApiKey}`)
+//       .then(response => response.json())
+//       .then(data => dispatch(loadTonightsShows(data.Events)))
+//   }
+// }
+
+
+//   try {
+//     // const response = await fetch(`http://api.jambase.com/events?zipCode=${zipCode}&radius=${radius}&page=0&api_key=${jambaseApiKey}`);
+//     const eventData = await response.json();
+//     // const cleanConcert = await cleanConcertData(eventData.Events);
+//     // const completedConcertObject = await fetchImage(cleanConcert);
+
+//     // return completedConcertObject;
+//     return eventData
+//   } catch (error) {
+//     throw error;
+//   }
+// };
--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import './index.css';
 import App from './containers/App/App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './reducers';
+import thunk from 'redux-thunk';
 
 const store = createStore(rootReducer, 
   window.__REDUX_DEVTOOLS_EXTENSION__ && 
