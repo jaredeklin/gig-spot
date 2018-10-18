a7c56e44ff3820a1dc14a773472bd871a537b12b
Jared Eklin <jaredeklin@gmail.com>
2018-10-18 17:34:27 -0600

    Add eventful image as default if no lastFm image

--- src/cleaners/cleanImage.js
+++ src/cleaners/cleanImage.js
@@ -1,9 +1,9 @@
 import defaultImage from '../images/black-woven.jpg';
 
-export const cleanImage = (artistData) => {
+export const cleanImage = (artistData, currentImage) => {
   if (!artistData.artist || !artistData.artist.image[4]) {
 
-    return defaultImage; 
+    return currentImage ? currentImage : defaultImage; 
   } else {
     const artistImage = artistData.artist.image[4][`#text`];
 
