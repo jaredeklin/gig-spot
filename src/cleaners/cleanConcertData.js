22b548913eac04b8632c5a3a8c0b4ecacb57bb15
Jared Eklin <jaredeklin@gmail.com>
2018-10-18 17:35:34 -0600

    Refactor cleaner to use eventful api

--- src/cleaners/cleanConcertData.js
+++ src/cleaners/cleanConcertData.js
@@ -1,37 +1,48 @@
 import { cleanTime } from './cleanTime';
+import { CleanArtists } from "./cleanArtists";
+
+const cleanArtists = new CleanArtists();
 
 export const cleanConcertData = (concerts) => {
 
   return concerts.reduce((concertArray, show) => {
+
     const venue = {
-      name: show.Venue.Name,
-      id: show.Venue.Id,
-      url: show.Venue.Url,
-      address: show.Venue.Address,
-      city: show.Venue.City
+      name: show.venue_name,
+      id: show.venue_id,
+      url: show.venue_url,
+      address: show.venue_address,
+      city: show.city_name,
+      state: show.region_abbr,
+      zip: show.postal_code
     };
-    const headlineArtist = show.Artists[0];
-    const supportArtists = show.Artists
-      .filter(artist => artist.Id !== show.Artists[0].Id);
-    const tickets = show.TicketUrl;
-    const id = show.Id;
-    const date = new Date(show.Date).toLocaleDateString([], {
+    const artists = cleanArtists.clean(show);
+    const headlineArtist = artists[0];
+    const supportArtists = artists.filter(artist => artist !== artists[0]);
+    const description = show.description;
+    const image = !show.image ? null : show.image.large.url;
+    const tickets = show.url;
+    const id = show.id;
+    const date = new Date(show.start_time).toLocaleDateString([], {
       month: 'short',
       day: 'numeric',
       year: 'numeric'
     });  
-
-    const startTime = cleanTime(show.Date);
+    const startTime = cleanTime(show.start_time);
     const concertData = {
       headlineArtist,
       supportArtists,
+      description,
       venue,
       date,
       startTime,
       id,
-      tickets
+      tickets,
+      image
     };
 
     return [...concertArray, concertData];
   }, []); 
 };
+
+
