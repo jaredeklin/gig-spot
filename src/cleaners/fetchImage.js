fd56073613368e2e408592baa67897b83ea88f64
Jared Eklin <jaredeklin@gmail.com>
2018-10-18 17:33:40 -0600

    Add eventful image as a parameter of cleanImage

--- src/cleaners/fetchImage.js
+++ src/cleaners/fetchImage.js
@@ -2,9 +2,9 @@ import { lastFmApiKey } from './apiKey';
 import { cleanImage } from './cleanImage';
 
 export const fetchImage = (concerts) => {
- 
   const promises = concerts.map( async (concert) => {
-    const artist = concert.headlineArtist.Name;
+
+    const artist = concert.headlineArtist;
     const baseUrl = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo';
     const keyUrl = `&artist=${artist}&api_key=${lastFmApiKey}&format=json`;
 
@@ -12,11 +12,12 @@ export const fetchImage = (concerts) => {
       const response = await fetch(`${baseUrl}${keyUrl}`);
       const artistData = await response.json();
 
-      return ({ ...concert, image: cleanImage(artistData) });
+      return ({ ...concert, image: cleanImage(artistData, concert.image) });
       
     } catch (error) {
       throw error;
     } 
+    
   });
 
   return Promise.all(promises); 
