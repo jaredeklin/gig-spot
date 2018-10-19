151f00b5a6bc1d91495876d7cfd438f6e196beb4
Jared Eklin <jaredeklin@gmail.com>
2018-10-19 15:26:20 -0600

    Add Query class to parse queries

---
+++ src/cleaners/Query.js
@@ -0,0 +1,18 @@
+import { eventfulApiKey } from "./apiKey";
+
+export class Query {
+  getUrl = (city) => {
+    const location = `&location=${city}`;
+    const images = '&image_sizes=large,block250';
+    const sortOrder = '&sort_order=popularity';
+    const resultLength = '&page_size=25';
+    const category = '&category=music';
+    const includes = "&include=categories,popularity,tickets,links";
+    const rootUrl = `http://api.eventful.com/json/events/search?`;
+    const query1 = `app_key=${eventfulApiKey}${location}${category}${images}`;
+    const query2 = `${sortOrder}${resultLength}${includes}`;
+    const url = `${rootUrl}${query1}${query2}`;
+
+    return url;
+  }
+}
