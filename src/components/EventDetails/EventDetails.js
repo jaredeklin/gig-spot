acf5a5f664298b932b3cfbffd27780a40f204d13
Jared Eklin <jaredeklin@gmail.com>
2018-10-20 14:36:15 -0600

    Update EventDetails to not show venue url

--- src/components/EventDetails/EventDetails.js
+++ src/components/EventDetails/EventDetails.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './eventDetails.css';
 import PropTypes from 'prop-types';
 
-export const EventDetails = ({concert}) => {
+export const EventDetails = ({ concert }) => { 
   const { 
     headlineArtist, 
     supportArtists, 
@@ -14,36 +14,32 @@ export const EventDetails = ({concert}) => {
   } = concert;
 
   const allSupportArtists = supportArtists.map(artist => {
-    return (<h4 className="support" key={artist.Id}>{artist.Name}</h4>);
+    return (<h4 className="support" key={artist+Date.now()}>{artist}</h4>);
   });
 
-  return (
-    <article className="event-details">
-      <img src={image} className="detail-img" alt="artist"/>
-      <div className="detail-right">
-        <h2 className="headliner">{headlineArtist.Name}</h2>
-        <div className="detail-date">{date}</div>
-        {
-          supportArtists.length > 0 &&  
-            <div className="support">With:
-              {allSupportArtists}
-            </div>
-        }
-        <div className="detail-venue">
+  return <article className="event-details">
+    <img src={image} className="detail-img" alt="artist" />
+    <div className="detail-right">
+      <h2 className="headliner">{headlineArtist}</h2>
+      <div className="detail-date">{date}</div>
+      {supportArtists.length > 0 && <div className="support">
+            With:
+        {allSupportArtists}
+      </div>}
+      <div className="detail-venue">
+        <a href={venue.url} target="_blank">
           <h4 className="venue-name">{venue.name}</h4>
-          <a href={venue.url} target="_blank">{venue.url}</a>
-          <div className="address">{venue.address}, {venue.city}</div>
+        </a>
+        <div className="address">
+          {venue.address}, {venue.city}
         </div>
-        <div className="detail-start-time">{startTime}</div>
-        {
-          tickets &&
-            <a href={tickets} className="ticket-info" target="_blank">
-              <h4>Get tickets</h4>
-            </a>
-        }
       </div>
-    </article>
-  );
+      <div className="detail-start-time">{startTime}</div>
+      {tickets && <a href={tickets} className="ticket-info" target="_blank">
+        <h4>Get tickets</h4>
+      </a>}
+    </div>
+  </article>;
 };
 
 EventDetails.propTypes = {
