50944c6804479f755ea0e454544f2f6ae2b8e37a
Jared Eklin <jaredeklin@gmail.com>
2018-10-20 15:33:26 -0600

    update event details format

--- src/components/EventDetails/EventDetails.js
+++ src/components/EventDetails/EventDetails.js
@@ -17,29 +17,34 @@ export const EventDetails = ({ concert }) => {
     return (<h4 className="support" key={artist+Date.now()}>{artist}</h4>);
   });
 
-  return <article className="event-details">
-    <img src={image} className="detail-img" alt="artist" />
-    <div className="detail-right">
-      <h2 className="headliner">{headlineArtist}</h2>
-      <div className="detail-date">{date}</div>
-      {supportArtists.length > 0 && <div className="support">
+  return (
+    <article className="event-details">
+      <img src={image} className="detail-img" alt="artist" />
+      <div className="detail-right">
+        <h2 className="headliner">{headlineArtist}</h2>
+        <div className="detail-date">{date}</div>
+        {supportArtists.length > 0 && 
+          <div className="support">
             With:
-        {allSupportArtists}
-      </div>}
-      <div className="detail-venue">
-        <a href={venue.url} target="_blank">
-          <h4 className="venue-name">{venue.name}</h4>
-        </a>
-        <div className="address">
-          {venue.address}, {venue.city}
+            {allSupportArtists}
+          </div>}
+        <div className="detail-venue">
+          <a href={venue.url} target="_blank">
+            <h4 className="venue-name">{venue.name}</h4>
+          </a>
+          <div className="address">
+            {venue.address}, {venue.city}
+          </div>
         </div>
+        <div className="detail-start-time">{startTime}</div>
+        {tickets && 
+          <a href={tickets} className="ticket-info" target="_blank">
+            <h4>Get tickets</h4>
+          </a>
+        }
       </div>
-      <div className="detail-start-time">{startTime}</div>
-      {tickets && <a href={tickets} className="ticket-info" target="_blank">
-        <h4>Get tickets</h4>
-      </a>}
-    </div>
-  </article>;
+    </article>
+  );
 };
 
 EventDetails.propTypes = {
