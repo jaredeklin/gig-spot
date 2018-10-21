8a894a000eba49a424d53ee0fe2205555d883ac0
Jared Eklin <jaredeklin@gmail.com>
2018-10-21 10:33:15 -0600

    Add message prop and proptypes to Loading

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import loadingGif from '../../images/spinning-7-inch.gif';
 
-export const Loading = () => {
+export const Loading = ({ message }) => {
   return (
     <div className="loading">
       <img src={loadingGif} className="loading-gif" alt="loading" />
-      <h2>Finding local shows....</h2>
+      <h2>{message}</h2>
     </div>
   );
+};
+
+Loading.propTypes = {
+  message: PropTypes.string
 };
