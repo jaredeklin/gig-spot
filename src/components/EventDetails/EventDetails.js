75a113ae43c7b06d025196cfc77dac62088ef7f0
Jared Eklin <jaredeklin@gmail.com>
2018-10-21 11:01:17 -0600

    Add error handleing for broken image links in EventDetails

--- src/components/EventDetails/EventDetails.js
+++ src/components/EventDetails/EventDetails.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import './eventDetails.css';
 import PropTypes from 'prop-types';
+import defaultImage from '../../images/black-woven.jpg';
+import './eventDetails.css';
 
 export const EventDetails = ({ concert }) => { 
   const { 
@@ -19,15 +20,19 @@ export const EventDetails = ({ concert }) => {
 
   return (
     <article className="event-details">
-      <img src={image} className="detail-img" alt="artist" />
+      <img 
+        src={image} 
+        onError={event => (event.target.src = defaultImage)} 
+        className="detail-img" 
+        alt="artist" 
+      />
       <div className="detail-right">
         <h2 className="headliner">{headlineArtist}</h2>
         <div className="detail-date">{date}</div>
-        {supportArtists.length > 0 && 
-          <div className="support">
-            With:
-            {allSupportArtists}
-          </div>}
+        {supportArtists.length > 0 && <div className="support">
+          With:
+          {allSupportArtists}
+        </div>}
         <div className="detail-venue">
           <a href={venue.url} target="_blank">
             <h4 className="venue-name">{venue.name}</h4>
@@ -37,11 +42,9 @@ export const EventDetails = ({ concert }) => {
           </div>
         </div>
         <div className="detail-start-time">{startTime}</div>
-        {tickets && 
-          <a href={tickets} className="ticket-info" target="_blank">
-            <h4>Get tickets</h4>
-          </a>
-        }
+        {tickets && <a href={tickets} className="ticket-info" target="_blank">
+          <h4>Get tickets</h4>
+        </a>}
       </div>
     </article>
   );
