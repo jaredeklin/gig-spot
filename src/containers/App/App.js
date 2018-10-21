863bbbf5575cb6fe4052e6f2294ce7c3b41cfca5
Jared Eklin <jaredeklin@gmail.com>
2018-10-21 10:43:22 -0600

    Refactor to include Loading and Error comoponents in App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,10 +4,11 @@ import { connect } from 'react-redux';
 import { withRouter, Route, NavLink } from 'react-router-dom';
 import Main from '../Main/Main';
 import { EventDetails } from '../../components/EventDetails/EventDetails';
-import loadingGif from '../../images/spinning-7-inch.gif';
 import { LandingPage } from '../../components/LandingPage/LandingPage';
 import PropTypes from 'prop-types';
 import { fetchShows, getStorage } from '../../actions/index';
+import { Loading } from '../../components/Loading/Loading';
+import { Error } from '../../components/Error/Error';
 
 export class App extends Component {
 
@@ -29,49 +30,37 @@ export class App extends Component {
   render() {
     const { location, loading, error } = this.props;
     
-    return (
-      <div className="App">
-        { location.pathname !== '/' &&
-        <header className="App-header">     
-          <h1><NavLink to="/" className="header-link">Gig Spot</NavLink></h1>
+    return <div className="App">
+      {location.pathname !== '/' && <header className="App-header">
+        <h1>
+          <NavLink to="/" className="header-link">
+                Gig Spot
+          </NavLink>
+        </h1>
 
-          {
-            location.pathname.includes('/event-details') &&
-              <div className="home">
-                <NavLink to="../main" className="home-button">Home</NavLink>
-              </div>
-          }
-        </header>
+        {location.pathname.includes('/event-details') && <div className="home">
+          <NavLink to="../main" className="home-button">
+                  Home
+          </NavLink>
+        </div>}
+      </header>}
+
+      <Route exact path="/" component={LandingPage} />
+      <Route exact path="/main" component={Main} />
+
+      {loading && <Loading message="Finding local shows...." />}
+      {error && <Error />}
+
+      <Route path={'/event-details/:id'} render={({ match }) => {
+        const concert = this.findMatch(match);
+
+        if (concert) {
+          return <EventDetails concert={concert} />;
         }
 
-        <Route exact path = "/" component={ LandingPage } />
-        <Route exact path = "/main" component={ Main } />
-
-        {
-          loading &&
-            <div className="loading">
-              <img src={ loadingGif } className="loading-gif" alt="loading"/>
-              <h2>Finding local shows....</h2>
-            </div>
-        }
-        {
-          error &&
-            <h2 className="error">
-              Nooooooooo!!!!!!! Something went wrong. Please try again.
-            </h2>
-        }
-        
-        <Route path={'/event-details/:id'} render={({ match }) => {
-          const concert = this.findMatch(match);
-
-          if (concert) {
-            return (<EventDetails concert={ concert } />);
-          }
-
-          return null;
-        }} />
-      </div>
-    );
+        return null;
+      }} />
+    </div>;
   }
 }
 
