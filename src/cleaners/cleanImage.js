a51292642e77915e43318c15c445dbc9804bff55
Jared Eklin <jaredeklin@gmail.com>
2018-10-22 12:32:00 -0600

    Improve lastfm artist search params and return concert obj if error in fetchImage

--- src/cleaners/cleanImage.js
+++ src/cleaners/cleanImage.js
@@ -2,7 +2,7 @@ import defaultImage from '../images/black-woven.jpg';
 
 export const cleanImage = (artistData, currentImage) => {
   if (!artistData.artist || !artistData.artist.image[4]) {
-
+    
     return currentImage ? currentImage : defaultImage; 
   } else {
     const artistImage = artistData.artist.image[4]['#text'];
--- src/cleaners/fetchImage.js
+++ src/cleaners/fetchImage.js
@@ -1,4 +1,3 @@
-// import { lastFmApiKey } from './apiKey';
 import { cleanImage } from './cleanImage';
 
 const lastFmApiKey = process.env.REACT_APP_LASTFM_KEY;
@@ -6,20 +5,20 @@ const lastFmApiKey = process.env.REACT_APP_LASTFM_KEY;
 export const fetchImage = (concerts) => {
   const promises = concerts.map( async (concert) => {
 
-    const artist = concert.headlineArtist;
+    const artist = concert.headlineArtist.replace(/&/gi, 'and');
     const baseUrl = 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo';
     const keyUrl = `&artist=${artist}&api_key=${lastFmApiKey}&format=json`;
 
     try {
       const response = await fetch(`${baseUrl}${keyUrl}`);
       const artistData = await response.json();
-
-      return ({ ...concert, image: cleanImage(artistData, concert.image) });
       
+      return ({ ...concert, image: cleanImage(artistData, concert.image) });
+
     } catch (error) {
-      throw error;
-    } 
-    
+      console.log(error); //eslint-disable-line
+      return concert;
+    }   
   });
 
   return Promise.all(promises); 
