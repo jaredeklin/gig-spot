39bf565b14ea775db0213e04855f8a3ec108edfc
Jared Eklin <jaredeklin@gmail.com>
2018-10-24 14:02:59 -0600

    Refactor Dates class to use moment.js

--- src/cleaners/Dates.js
+++ src/cleaners/Dates.js
@@ -1,34 +1,21 @@
+const moment = require('moment');
+
 export class Dates {
   getDates() {
-    const today = new Date().toLocaleDateString([], {
-      month: 'short',
-      day: 'numeric',
-      year: 'numeric'
-    });
-    const tommorrow = new Date();
-    const nextWeek = new Date();
-    const upcoming = new Date();
-    const upcomingEnd = new Date();
-    tommorrow.setDate(tommorrow.getDate() + 1);
-    nextWeek.setDate(nextWeek.getDate() + 7);
-    upcoming.setDate(upcoming.getDate() + 8);
-    upcomingEnd.setMonth(upcomingEnd.getMonth() + 3);
-
     return {
-      today,
-      tommorrow: this.formatDate(tommorrow),
-      nextWeek: this.formatDate(nextWeek),
-      upcoming: this.formatDate(upcoming),
-      upcomingEnd: this.formatDate(upcomingEnd)
+      today: moment().format('YYYYMMDD00'),
+      tommorrow: moment()
+        .add(1, 'days')
+        .format('YYYYMMDD00'),
+      nextWeek: moment()
+        .add(7, 'days')
+        .format('YYYYMMDD00'),
+      upcoming: moment()
+        .add(8, 'days')
+        .format('YYYYMMDD00'),
+      upcomingEnd: moment()
+        .add(3, 'months')
+        .format('YYYYMMDD00')
     };
   }
-
-  formatDate = date => {
-    return (
-      date
-        .toISOString()
-        .substr(0, 10)
-        .replace(/[-]/gi, '') + '00'
-    );
-  };
 }
